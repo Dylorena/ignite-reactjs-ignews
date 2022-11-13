97c82856edd1cc073e0b301fdfad47add12b5a90
Dylorena <dilorenasantos@gmail.com>
2022-11-13 11:43:03 -0300

    add authentication

--- src/components/SignButton/index.tsx
+++ src/components/SignButton/index.tsx
@@ -1,18 +1,19 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
-    const isUserLoggedIn = true
+    const { data: session, status } = useSession()
 
-    return isUserLoggedIn ? (
-        <button type='button' className={styles.signInButton}>
+    return session ? (
+        <button type='button' className={styles.signInButton} onClick={() => signOut()}>
             <FaGithub color="#04d361" />
-            Diana Lorena
+            {session.user.name}
             <FiX color="#737378" className={styles.closeIcon} />
         </button>
     ) : (
-        <button type='button' className={styles.signInButton}>
+        <button type='button' className={styles.signInButton} onClick={() => signIn('github')}>
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,29 @@
+import { signIn, useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
-    priceId: string
+  priceId: string
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-    return (
-        <button
-            type="button"
-            className={styles.subscribeButton}
-        >
-            Subscribe Now
-        </button>
-    )
+  const { data: session, status } = useSession()
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe Now
+    </button>
+  )
 }
